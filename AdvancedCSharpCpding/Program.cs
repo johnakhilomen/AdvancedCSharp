c794144a2fa7e34af6cec357fa1696378fdf7b99
John Akhilomen <jonbasanti@gmail.com>
2020-10-13 09:50:24 -0500

    Added for NullTypes

---
+++ AdvancedCSharpCpding/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdvancedCSharpCpding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NullableTypes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NullableTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime? date = null;
+            //date = date.HasValue == false ? DateTime.Today : date.GetValueOrDefault();
+            //OR
+            date = date ?? DateTime.Today; 
+            Console.WriteLine("Date: " + date);
+            string? someName = null;
+            someName = someName ?? "Jane";
+            Console.WriteLine(someName);
+        }
+    }
+}
